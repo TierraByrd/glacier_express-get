5c97003c1b4af91b72c231122874e954ff8e3645
Tierra Byrd <tierrabyrd1@gmail.com>
2024-05-08 10:00:45 -0500

    working together in class

--- server/server.js
+++ server/server.js
@@ -11,6 +11,36 @@ const port = 5000
   // Tell the server where the files for the page are
 app.use(express.static('server/public'))
 
+
+
+let quoteList = [
+  { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+  { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+  { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
+
+// GET ROUTE
+
+ //app.use can be used for other things like routes
+ //route start with a method type: GET, POST, PUT , DELETE
+ //route need to have a path
+//route needs to have a callback function(arrow function)
+// route is a method type that receive two arguments
+// ALL ROUTES MUST RESPOND WITH EITHER A STATUS CODE OR DATA
+//mean we're calling the function app.get and telling it to call it quotes and what to do with the () {}
+
+// sending status number
+//app.get('/quotes', (req, res) => {
+ // res.sendStatus(200) // code for the endpoint
+// })
+
+//sending some data
+resizeBy.send(quoteList)
+
+
+
+
 // ! Actually start server when file is ran
 app.listen(
     port,
